7b841ceab4cb911a43b87cf8e01027b65d539e51
Murilo <murilo_marques09@hotmail.com>
2015-09-21 15:51:06 -0300

    Fundo e chão de teste

--- main.lua
+++ main.lua
@@ -7,29 +7,29 @@ local y = display.contentCenterY
 
 local game = display.newGroup( )
 
-local bkg = display.newImage("Img/bkg_fundo2.png")
+local bkg = display.newImage("Img/fundo_teste.png")
 bkg.x = x
 bkg.y = y
 game:insert(bkg)
 
-local circle = display.newCircle(  -0.5 , 600, 15 )
-physics.addBody( circle, "dinamic", {density = 1.0, friction = 0.3, bounce = 0.2 ,
-radius = 40	} )
+local circle = display.newCircle(  -14 , 600, 25 )
+physics.addBody( circle, "dinamic", {density = 8.0, friction = 0.3, bounce = 0.2 ,
+radius = 25	} )
 game:insert( circle )
 
 local canhao = display.newImage("Img/canhao2.png")
 canhao.x = 1
-canhao.y = 600
+canhao.y = 610
 game:insert( canhao )
 
-local floor = display.newImage( "Img/bkg_chao.jpg" )	
+local floor = display.newImage( "Img/chao_teste.png" )	
 floor.x = 200
 floor.y = 720
 physics.addBody( floor, "static",{density = 1.0, friction = 2.0, bounce = 0.1} )
 game:insert(floor)
 
 
---for i = 0, 0 do
+--for i = 0, 15 do
 
 --	local rect = display.newRect(800, 260 + i*30, 100, 40)
 --	physics.addBody( rect, "dinamic", {density = 1.0, friction = 0.3, bounce = 0.2} )
